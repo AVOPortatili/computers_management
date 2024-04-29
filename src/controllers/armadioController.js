105f256b093a12c74add9f25ce59ee92a0d14029
mario <mar.no.barb@gmail.com>
2024-04-29 12:34:04 +0200

    armadioController not working

---
+++ src/controllers/armadioController.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { pool } from '../mysqlConnector.js'
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    const ret = pool.query("select * from Armadi;", req.params.id ,function (error, results, fields) {
+        if(error) throw error;
+        console.log(results);
+        res.status(200).json(results);
+    });
+
+})
+
+
+router.get("/:id", (req, res) => {
+    const ret = pool.query("SELECT COUNT(Computers.id) FROM Armadi INNER JOIN Computers ON Computers.id = Armadi.id WHERE Computers.id IS NOT NULL;", req.params.id ,function (error, results, fields) {
+        if(error) throw error;
+        console.log(results);
+        res.status(200).json(results[0]);
+    });
+});
+
+export default router;
